1aa67b55f196b0ab13302a5b153ffd80d68b22ca
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2020-05-26 14:03:34 -0300

    terraform: WIP Resource Dependencies

--- terraform/example.tf
+++ terraform/example.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
