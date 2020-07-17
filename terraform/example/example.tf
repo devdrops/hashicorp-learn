3226725c7890e98d6a40390799d579f24aeec1df
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2020-07-17 11:03:00 -0300

    terraform: dependencies

---
+++ terraform/example/example.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t3a.small"
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
